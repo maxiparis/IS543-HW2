2e083c2e58d1c7e60a68a6c74829c867017e013a
“Max <mparis@hotmail.cl>
2024-09-10 08:56:07 -0600

    Initial Commit

---
+++ IS543-HW2/IS543_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  IS543_HW2App.swift
+//  IS543-HW2
+//
+//  Created by Maximiliano París Gaete on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IS543_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
